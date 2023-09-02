a5a99faddf78908bd6ab758846d9dcbb93ca7f53
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2023-09-02 15:54:45 -0300

    Upgrade basic estructure. -codigo: add global constants for spreadsheets. -modals: new file for create modals. -web: complete basic estructure.

--- codigo.js
+++ codigo.js
@@ -11,3 +11,9 @@ function doGet() {
 function includes(fileName) {
     return HtmlService.createHtmlOutputFromFile(fileName).getContent();
 }
+
+// Globals constants
+
+const SS_DATABASE = SpreadsheetApp.openById('1VQj_Q94xvpWtFb847UUzPl6d_AdU_XNU6Fifroo1b6o');
+const SS_REGISTER = SpreadsheetApp.openById('1grE9uiWBYmTrCQnfOPAWplegW2PjcA3xcL4_xfTqnVs');
+
